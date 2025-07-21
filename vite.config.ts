b7a6091816ba72b55ae0cfa640b08e23de4ca0ba
Swadesh Behera <itswadesh@gmail.com>
2025-07-21 19:13:50 +0530

    -

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import devtoolsJson from 'vite-plugin-devtools-json'
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
   preview: {
-    allowedHosts: ['metrics.litekart.in'],
+    allowedHosts: [
+      'metrics.litekart.in',
+      'misiki-litekartmetrics-aerecd-0ced29-69-62-81-230.traefik.me',
+    ],
   },
 })
