452ddc5c9ee5207b820b03780b4f55ce8033a215
Swadesh Behera <itswadesh@gmail.com>
2025-07-21 18:34:44 +0530

    -

--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,4 @@ export default defineConfig({
   preview: {
     allowedHosts: ['metrics.litekart.in'],
   },
-  server: {
-    allowedHosts: true, // This is required, else will "throw Blocked request. This host ("shopnx.in") is not allowed."
-    host: true,
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://api.sparkinity.com', // Backend server URL
-    //     changeOrigin: true, // Required for CORS
-    //     secure: false, // Disable SSL verification if needed
-    //     rewrite: (path) => path.replace(/^\/api/, 'api'), // Remove `/api` prefix
-    //   },
-    // },
-  },
 })
