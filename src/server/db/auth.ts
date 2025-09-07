723dd52c0313f9bef6074eb8e2f7458e64202973
Suprada-2002 <supradatripathy789@gmail.com>
2025-09-07 22:50:42 +0530

    fix: validate sesstion token fixed

--- src/server/db/auth.ts
+++ src/server/db/auth.ts
@@ -41,8 +41,6 @@ export async function validateSessionToken(token: string) {
           phone: User.phone,
           role: User.role,
           name: User.name,
-          board: User.board,
-          class: User.class,
         },
         session: Session,
       })
