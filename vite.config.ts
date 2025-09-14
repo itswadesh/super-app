2ed246452161f3a36fde3e243e1802ab967a03b2
Swadesh Behera <itswadesh@gmail.com>
2025-09-14 20:11:06 +0530

    -

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     allowedHosts: [
       'metrics.litekart.in',
       'misiki-litekartmetrics-aerecd-0ced29-69-62-81-230.traefik.me',
+      'misiki.in',
     ],
   },
 })
